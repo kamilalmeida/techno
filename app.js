f149fdb2b78c378495b2c809b3faa573f2d38164
Kamila <kamilaalmeida572@gmail.com>
2022-11-08 18:05:22 -0300

    ajuste de layout

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 new Vue({
-  el:"#app",
-  data:{
-    nome:"ka"
-  }
-})
+  el: "#app",
+  data: {
+    products: [],
+    product: false,
+  },
+  filters: {
+    numberPrice(value) {
+      return value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+  methods: {
+    fetchProducts() {
+      fetch("./api/products.json")
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          this.products = response;
+        });
+    },
+    fetchProduct(id) {
+      console.log(id);
+      fetch(`./api/products/${id}/data.json`)
+        .then((response) => response.json())
+        .then((response) => {
+          this.product = response;
+        });
+    },
+  },
+  created() {
+    this.fetchProducts();
+  },
+});
