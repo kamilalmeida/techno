17469ad3b0af7d4196e09e2d6217f729fd95ef4a
Kamila <kamilaalmeida572@gmail.com>
2022-11-09 11:43:58 -0300

    ajustando layout do carrinho

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ new Vue({
   data: {
     products: [],
     product: false,
-    fullCart: 0,
+    cart: [],
+    activeCart: false,
+    alertMessage: "Item adicionado",
+    activeAlert: false,
   },
   filters: {
     numberPrice(value) {
@@ -11,6 +14,17 @@ new Vue({
         style: "currency",
         currency: "BRL",
       });
+    },
+  },
+  computed: {
+    fullCart() {
+      let total = 0;
+      if (this.cart.length) {
+        this.cart.forEach((item) => {
+          total += item.price;
+        });
+      }
+      return total;
     },
   },
   methods: {
@@ -37,8 +51,50 @@ new Vue({
     closeModal({ target, currentTarget }) {
       if (target === currentTarget) this.product = false;
     },
+    clickOutCart({ target, currentTarget }) {
+      if (target === currentTarget) this.activeCart = false;
+    },
+    addItem() {
+      this.product.stock--;
+      const { id, name, price } = this.product;
+      this.cart.push({ id, name, price });
+      this.alert(`${name} foi adicionado ao carrinho`);
+    },
+    removeItem(index) {
+      this.cart.splice(index, 1);
+    },
+    checkLocalStorage() {
+      if (window.localStorage.cart)
+        this.cart = JSON.parse(window.localStorage.cart);
+    },
+    alert(message) {
+      this.alertMessage = message;
+      this.activeAlert = true;
+      setTimeout(() => (this.activeAlert = false), 1500);
+    },
+    checkStock() {
+      const items = this.cart.filter((item) => {
+        if (item.id === this.product.id) {
+          return true;
+        }
+      });
+
+      this.product.stock -= items.length;
+    },
+  },
+  watch: {
+    product() {
+      document.title = this.product.name || "Techno";
+      const id = this.product.id || " ";
+      history.pushState(null, null, `#${id}`);
+      if (this.product) this.checkStock();
+    },
+    cart() {
+      window.localStorage.cart = JSON.stringify(this.cart);
+    },
   },
   created() {
     this.fetchProducts();
+    this.checkLocalStorage();
   },
 });
